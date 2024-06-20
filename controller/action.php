bc4b81854258f4d72a5c52ae85eba47e2611425d
Rakantamir <rakantrengganatamir@smkwikrama.sch.id>
2024-06-20 21:22:58 +0700

    rute-penerbangan/controller/action.php

--- controller/action.php
+++ controller/action.php
@@ -15,4 +15,31 @@ $dataPajak = json_decode(file_get_contents("../data/pajak.json"), true);
 $key = array_search($tujuan_penerbangan, array_column($dataPajak[1]['tujuan'], 'inisial'));
 $tujuan_penerbangan = $dataPajak[1]['tujuan'][$key]['nama_bandara'];
 $pajak_tujuan_penerbangan = $dataPajak[1]['tujuan'][$key]['pajak'];
+
+//Menjumlahkan pajak dan menentukan total harga
+$pajak = $pajak_asal_penerbangan + $pajak_tujuan_penerbangan;
+$total_harga = $harga_tiket + $pajak;
+
+//keep data from data.json
+$data = json_decode(file_get_contents('../data/data.json'), true);
+
+//menyimpan ke json
+$data[] = array(
+    'nama_maskapai' => $nama_maskapai,
+    'asal_penerbangan' => $asal_penerbangan,
+    'tujuan_penerbangan' => $tujuan_penerbangan,
+    'harga_tiket' => $harga_tiket,
+    'pajak' => $pajak,
+    'total_harga' => $total_harga
+);
+
+// meng-encode data menjadi json
+$jsonfile = json_encode($data, JSON_PRETTY_PRINT);
+
+//menyimpan data ke dalam anggota-json
+file_put_contents('../data/data.json', $jsonfile);
+
+header('location:../index.php');
+exit();
+
 ?>
